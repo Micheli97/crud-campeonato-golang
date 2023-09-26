fad75a6cc6d24d6be0f9c5139d8182716ae7e6ba
Paula Micheli <56922971+Micheli97@users.noreply.github.com>
2023-09-25 21:56:43 -0300

    feat(): Criando estruturas para tratamento de error

---
+++ config/error/rest_error.go
@@ -0,0 +1,76 @@
+package error
+
+import "net/http"
+
+type RestErr struct {
+	Message string   `json:"message"`
+	Err     string   `json:"err"`
+	Code    int64    `json:"code"`
+	Causes  []Causes `json:"causes"`
+}
+
+type Causes struct {
+	Field   string `json:"field"`
+	Message string `json:"message"`
+}
+
+// Error retorna a mesagem de error
+func (r *RestErr) Error() string {
+	return r.Message
+}
+
+// Retorna error
+func NewRestErr(message, err string, code int64, causes []Causes) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     err,
+		Code:    code,
+		Causes:  causes,
+	}
+}
+
+// NewUnauthorizedRequestError retorna erro de autorização
+func NewUnauthorizedRequestError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "unauthorized",
+		Code:    http.StatusUnauthorized,
+	}
+}
+
+// NewBadRequestValidationError retorna erro de validação
+func NewBadRequestValidationError(message string, causes []Causes) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "bad_request",
+		Code:    http.StatusBadRequest,
+		Causes:  causes,
+	}
+}
+
+// NewInternalServerError retorna erro interno do servidor
+func NewInternalServerError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "internal_server_error",
+		Code:    http.StatusInternalServerError,
+	}
+}
+
+// NewNotFoundError retorna um erro não encontrado
+func NewNotFoundError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "not_found",
+		Code:    http.StatusNotFound,
+	}
+}
+
+// NewForbiddenError retora erro de não autorizado
+func NewForbiddenError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "forbidden",
+		Code:    http.StatusForbidden,
+	}
+}
