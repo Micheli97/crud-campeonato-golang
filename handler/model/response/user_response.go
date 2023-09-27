0ecdf88b6301408d857600811c361cf2c5239616
Paula Micheli <56922971+Micheli97@users.noreply.github.com>
2023-09-27 14:40:50 -0300

    refactor(): Removendo campo do objeto user response

--- handler/model/response/user_response.go
+++ handler/model/response/user_response.go
@@ -1,8 +1,7 @@
 package response
 
 type UserResponse struct {
-	ID       string `json:"ID"`
-	Name     string `json:"name"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	ID    string `json:"ID"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
 }
