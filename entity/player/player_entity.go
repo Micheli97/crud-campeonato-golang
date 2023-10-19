15b02210870bea70cdcd10170398e06bf257832c
Paula Micheli <56922971+Micheli97@users.noreply.github.com>
2023-10-19 11:08:11 -0300

    feat(): Adicionando player entity

---
+++ entity/player/player_entity.go
@@ -0,0 +1,13 @@
+package player
+
+type PlayerEntity struct {
+	Id       string
+	Name     string
+	Photo    string
+	Height   float32
+	Weight   float32
+	Age      int32
+	Position string
+	Number   int32
+	TeamID   string
+}
