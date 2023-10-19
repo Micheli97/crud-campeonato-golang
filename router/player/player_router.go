b1353e331e7a5d5ee73ff1d8943894887c748bce
Paula Micheli <56922971+Micheli97@users.noreply.github.com>
2023-10-19 11:09:04 -0300

    feat(): Adicionando router

---
+++ router/player/player_router.go
@@ -0,0 +1,27 @@
+package player
+
+import "github.com/gin-gonic/gin"
+
+func PLayerRouter(router *gin.RouterGroup, playerHandler player.PlayerHandlerInterface) {
+
+	playerGroup := router.Group("/player")
+
+	// Listar jogadores cadastrados
+	playerGroup.GET("/", playerHandler.FindPlayers)
+
+	// Lista a quantidade de jogadores cadastrados
+	playerGroup.GET("/countPlayer", playerHandler.FindTotalPlayer)
+
+	// Cadastra um novo jogador
+	playerGroup.POST("/", playerHandler.CreatePlayer)
+
+	// Atualiza os dados do jogador
+	playerGroup.PUT("/:id", playerHandler.UpdatePlayer)
+
+	// Deleta jogador
+	playerGroup.DELETE("/:id", playerHandler.DeletePlayer)
+
+	// Lista jogador pelo ID
+	playerGroup.GET("/:id", playerHandler.FindPlayerByID)
+
+}
