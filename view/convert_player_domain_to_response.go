fa5814c55b22424ec86a79fc0005bbc9f299ebcd
Paula Micheli <56922971+Micheli97@users.noreply.github.com>
2023-10-19 11:10:08 -0300

    feat(): Criando router e view player

---
+++ view/convert_player_domain_to_response.go
@@ -0,0 +1,17 @@
+package view
+
+import "github.com/Micheli97/crud-campeonato-golang/domain/player"
+
+func ConvertPlayerDomainToResponse(playerDomain player.PlayerDomainInterface) request.PlayerRequest {
+	return request.PlayerRequest{
+		Id:       playerDomain.GetID(),
+		Weight:   playerDomain.GetWeight(),
+		Name:     playerDomain.GetName(),
+		Height:   playerDomain.GetHeight(),
+		Number:   playerDomain.GetNumber(),
+		Photo:    playerDomain.GetPhoto(),
+		Position: playerDomain.GetPosition(),
+		Age:      playerDomain.GetAge(),
+		TeamID:   playerDomain.GetTeamID(),
+	}
+}
